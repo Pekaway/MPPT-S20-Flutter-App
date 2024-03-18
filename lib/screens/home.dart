2963caf2f7c12d64b287ea81b14a4ae82692e1da
Willi Preuk <willi@preuk.eu>
2024-03-18 20:02:34 +0100

    basic bluetooth search

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,6 +11,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  scan() async {
+    FlutterBluePlus.setLogLevel(LogLevel.verbose, color:false);
+    // first, check if bluetooth is supported by your hardware
+// Note: The platform is initialized on the first call to any FlutterBluePlus method.
+    if (await FlutterBluePlus.isSupported == false) {
+      print("Bluetooth not supported by this device");
+      return;
+    }
+
+// turn on bluetooth ourself if we can
+// for iOS, the user controls bluetooth enable/disable
+    if (Platform.isAndroid) {
+      await FlutterBluePlus.turnOn();
+    }
+
+    // listen to scan results
+// Note: `onScanResults` only returns live scan results, i.e. during scanning
+// Use: `scanResults` if you want live scan results *or* the results from a previous scan
+    var subscription = FlutterBluePlus.onScanResults.listen((results) {
+      if (results.isNotEmpty) {
+        ScanResult r = results.last; // the most recently found device
+        print('${r.device.remoteId}: "${r.advertisementData.advName}" found!');
+      }
+    },
+      onError: (e) => print(e),
+    );
+
+// cleanup: cancel subscription when scanning stops
+    FlutterBluePlus.cancelWhenScanComplete(subscription);
+
+// Wait for Bluetooth enabled & permission granted
+// In your real app you should use `FlutterBluePlus.adapterState.listen` to handle all states
+    await FlutterBluePlus.adapterState.where((val) => val == BluetoothAdapterState.on).first;
+
+// Start scanning w/ timeout
+// Optional: you can use `stopScan()` as an alternative to using a timeout
+// Note: scan filters use an *or* behavior. i.e. if you set `withServices` & `withNames`
+//   we return all the advertisments that match any of the specified services *or* any
+//   of the specified names.
+    await FlutterBluePlus.startScan(
+        withNames:["SolarLife"],
+        timeout: Duration(seconds:15));
+
+// wait for scanning to stop
+    await FlutterBluePlus.isScanning.where((val) => val == false).first;
+    print("finished scanning");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +76,11 @@ class _HomeState extends State<Home> {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: scan,
+        tooltip: 'Scan',
+        child: const Icon(Icons.bluetooth_searching),
+      )
     );
   }
 }
